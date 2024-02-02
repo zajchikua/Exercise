58019fddfbd6be054853a87b9428af5b13057525
anastasiya <miroshnychenkoa@yahoo.com>
2024-02-02 09:34:10 -0800

    Recursive factorial, sum of Natural numbers

--- src/Recursion.java
+++ src/Recursion.java
@@ -24,15 +24,29 @@ public class Recursion {
         System.out.println( i);
     }
 
-    public static void factorial(int number){
-        if(number < 1) return;
-        System.out.println(number);
-        factorial( number * ( number- 1));
+    public static int factorial(int number){
+        if(number <= 1) return 1;
+        return number * factorial(  number- 1);
+    }
+    /*
+    Exercise 1: Sum of Natural Numbers
+Write a recursive Java method to calculate the sum of the first n natural numbers.
+     */
+
+    public static int naturalSum(int n)
+    {
+        if(n == 0){
+            return 0;
+        }
+        return n + naturalSum(n-1);
+
     }
     public static void main(String[] args) {
-        printNtimes("Miro & Evangelina", 5);
+       // printNtimes("Miro & Evangelina", 5);
         //printNTimes("Miro & Evangelina", 5, 0);
-printNtimes1("Miro", 5);
-factorial(5);
+        //printNtimes1("Miro", 5);
+        //System.out.println(factorial(5));
+
+        System.out.println(naturalSum(5));
     }
 }
