da36667c160bb506ef791e6a226250239985ea5f
anastasiya <miroshnychenkoa@yahoo.com>
2024-02-12 23:04:02 -0800

    Continue Uno card game, Game class

--- src/Games/Uno/Game.java
+++ src/Games/Uno/Game.java
@@ -1,6 +1,7 @@
 package Games.Uno;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Game {
     private int currentPlayer;
@@ -14,4 +15,38 @@ public class Game {
     private UnoCard.Value validValue;
 
     boolean gameDirection;
+
+    public Game(String[] pids)
+    {
+        deck = new UnoDeck();
+        deck.shuffle();
+        stockpile = new ArrayList<UnoCard>();
+
+        playersIds = pids;
+        currentPlayer = 0;
+        gameDirection = false;
+
+        playerHand = new ArrayList<ArrayList<UnoCard>>();
+        //fill up the players with uno cards
+        for(int i = 0; i < pids.length; i++)
+        {
+           ArrayList<UnoCard> hand = new ArrayList<UnoCard>(Arrays.asList(deck.drawCard(7)));
+           playerHand.add(hand);
+        }
+    }
+
+    public void start(Game game)
+    {
+        UnoCard card = deck.drawCard();
+        validColor = card.getColor();
+        validValue = card.getValue();
+
+        if (card.getValue() == UnoCard.Value.Wild_Four ||card.getValue() == UnoCard.Value.DrawTwo)
+        {
+            start(game);
+        }
+
+
+
+    }
 }
